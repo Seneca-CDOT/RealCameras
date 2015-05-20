6560c987ea20733cd2bdd1a61498df86b7765f8d
barbara <barbara@cdotadmins-MacBook-Pro.local>
2015-05-20 13:10:33 -0400

    Working test files

---
+++ cameraTest.js
@@ -0,0 +1,51 @@
+//a test js file to make a scene with a camera
+//make a camera and some cubes and find the values of various camera propetires
+
+var container, canvas;
+var camera, scene, renderer;
+var cube1, cube2;
+
+function init() {
+	var canvasWidth = window.innerWidth;
+	var canvasHeight = window.innerHeight;
+	var canvasRatio = canvasWidth/canvasHeight;
+
+	renderer = new THREE.WebGLRenderer();
+	renderer.setSize(canvasWidth, canvasHeight);
+
+	camera = new THREE.PerspectiveCamera(45, canvasRatio, 1, 100);
+	camera.position.set(0,10,4);
+
+	scene = new THREE.Scene();
+
+	var light = new THREE.DirectionalLight(0xFFFFFF, 1.0);
+	light.position.set(200,400,500);
+	scene.add(light);
+
+	cube1 = new THREE.Mesh(new THREE.CubeGeometry(2,5,2), new THREE.MeshNormalMaterial());
+	cube1.position.set(20,0,0);
+
+	cube2 = new THREE.Mesh(new THREE.CubeGeometry(2,10,2), new THREE.MeshNormalMaterial());
+	cube2.position.set(40,0,0);	
+
+	scene.add(cube1);
+	scene.add(cube2);
+
+	camera.lookAt(cube1.position);
+	document.body.appendChild(renderer.domElement);
+
+	window.alert("Camera"+ camera.rotation.x);
+}
+
+function animate(){
+	window.requestAnimationFrame(animate);
+	render();
+}
+
+function render(){
+	renderer.render(scene,camera);
+}
+
+	init();
+	animate();
+
