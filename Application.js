d43f6e2ac57ae49055bae4e1386a0023fae960ef
Andrii Bugaiov <andriibugaiov@Senecas-iMac-2.local>
2015-08-12 18:45:39 -0400

    human, progress control

--- Application.js
+++ Application.js
@@ -87,6 +87,7 @@ var Application = (function () {
             root.appendChild(store.camDescription.container);
 
             var sl = Application.SceneLoader.getInstance();
+            sl.setProgressControlContainer(store.demonstrator.container);
             sl.load().then(function (meshesContainer) {
                 store.demonstrator.setUpScene(meshesContainer);
                 
